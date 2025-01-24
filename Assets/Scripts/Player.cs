48a9acff7af538d82c22df600a87f22a502a6b9f
azzinoth01 <markusdullnig@hotmail.com>
2025-01-24 21:01:21 +0100

    basic Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour, PlayerAction.IPlayerInputActions
+{
+    private Vector2 _moveDirection;
+    [SerializeField] private float _moveForce;
+    [SerializeField] private float _maxMoveSpeed;
+
+    private Rigidbody2D _body;
+
+    private PlayerAction _inputControl;
+
+    public void OnMove(InputAction.CallbackContext context) {
+        _moveDirection = context.ReadValue<Vector2>();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+        if (_inputControl == null) {
+            _inputControl = new PlayerAction();
+            _inputControl.PlayerInput.Enable();
+            _inputControl.PlayerInput.SetCallbacks(this);
+        }
+        _body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        _body.AddForce(_moveForce * _moveDirection * Time.deltaTime);
+
+        _body.linearVelocity = _body.linearVelocity.normalized * Mathf.Clamp(_body.linearVelocity.magnitude, 0, _maxMoveSpeed);
+
+    }
+
+    private void OnDestroy() {
+        if (_inputControl != null) {
+            _inputControl.Dispose();
+            _inputControl = null;
+        }
+    }
+}
