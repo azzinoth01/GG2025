d8ae6f504bb95dbb8ba999a4a214922eaee37c74
Martin Viidik <martinviidik@gmail.com>
2025-01-24 22:47:56 +0100

    Add ammo recharge

--- Assets/Scripts/Player/PlayerAmmoManager.cs
+++ Assets/Scripts/Player/PlayerAmmoManager.cs
@@ -1,11 +1,16 @@
+using System;
+using NUnit.Framework;
 using UnityEngine;
 
 public class PlayerAmmoManager : MonoBehaviour
 {
     [SerializeField] private float maxAmmo;
     [SerializeField] private float depleteAmount;
+    [SerializeField] private float rechargeAmount;
+    [SerializeField] private float rechargeDelay;
 
     private float currentAmmo;
+    private float currentTime;
     
     void Start(){
         currentAmmo = maxAmmo;
@@ -17,9 +22,19 @@ public class PlayerAmmoManager : MonoBehaviour
     public void DepleteAmmo(){
         currentAmmo -= depleteAmount;
         Debug.Log("Ammo left: " + currentAmmo);
+        currentTime = 0.0f;
     }
     public void AddAmmo(float toAdd){
         currentAmmo += toAdd;
+        Debug.Log("Ammo left: " + currentAmmo);
+        currentTime = 0.0f;
     }
-
+    private void Update(){
+        if(currentAmmo != maxAmmo){
+            currentTime += Time.deltaTime;
+            if(currentTime >= rechargeDelay){
+                AddAmmo(rechargeAmount);
+            }
+        }
+    }
 }
