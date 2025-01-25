4abe3948bc7f91954351ce310810ae0ba0181fd2
Martin Viidik <martinviidik@gmail.com>
2025-01-25 11:15:30 +0100

    Adding invulnerable time

--- Assets/Scripts/Player/PlayerHealthManager.cs
+++ Assets/Scripts/Player/PlayerHealthManager.cs
@@ -4,20 +4,60 @@ using UnityEngine;
 public class PlayerHealthManager : MonoBehaviour, IHealthInterface
 {
     [SerializeField] private float playerHealthMax = 100;
+    [SerializeField] private float invulnerableDelay = 2;
     private float playerHealth;
+    private float invulnerableTime;
+
+    public event EventHandler<EventHandler> OnPlayerDied;
+    public event EventHandler<OnHealthChangedEventArgs> OnHealthChanged;
+    public class OnHealthChangedEventArgs : EventArgs {
+        public float health;
+    }
+
+    public event EventHandler<OnSetIlvunerableEventArgs> SetIlvunerable;
+    public class OnSetIlvunerableEventArgs : EventArgs {
+        public bool isInvulnerable;
+    }
+
+    private enum HealthState {Alive, Invulnerable, Dead};
+    private HealthState currentHealthState;
 
     public void KillActor(){
-        Destroy(gameObject);
+        gameObject.SetActive(false);
     }
-
     public void UpdateHealth(float amount){
-        playerHealth -= amount;
-        if(playerHealth <= 0){
-            KillActor();
+        if(amount > 0 && currentHealthState != HealthState.Invulnerable){
+            playerHealth -= amount;
+            OnHealthChanged?.Invoke(this, new OnHealthChangedEventArgs{ health = playerHealth / playerHealthMax });
+            currentHealthState = HealthState.Invulnerable;
+
+            if(playerHealth <= 0){
+                KillActor();
+                currentHealthState = HealthState.Dead;
+            }
         }
     }
-
     void Start(){
         playerHealth = playerHealthMax;
+        currentHealthState = HealthState.Alive;
+    }
+    void Update(){
+        switch(currentHealthState){
+            case HealthState.Alive:
+                break;
+            case HealthState.Invulnerable:
+                invulnerableTime += Time.deltaTime;
+                Debug.Log(invulnerableTime);
+                SetIlvunerable?.Invoke(this, new OnSetIlvunerableEventArgs{ isInvulnerable = true});
+                if(invulnerableTime >= invulnerableDelay){
+                    currentHealthState = HealthState.Alive;
+                    SetIlvunerable?.Invoke(this, new OnSetIlvunerableEventArgs{ isInvulnerable = false});
+                    invulnerableTime = 0f;
+                }
+                break;
+            case HealthState.Dead:
+                KillActor();
+                break;
+        }
     }
 }
---
+++ Assets/Scripts/UI/HealthCounter.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthCounter : MonoBehaviour
+{
+    [SerializeField] private Image healthFill;
+    [SerializeField] private Player player;
+    private PlayerHealthManager playerHealthManager;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        healthFill.fillAmount = 1.0f;
+
+        playerHealthManager = player.GetComponent<PlayerHealthManager>();
+        
+        playerHealthManager.OnHealthChanged += OnHealthChanged;
+        playerHealthManager.SetIlvunerable += OnSetIlvunerable;
+    }
+
+    private void OnSetIlvunerable(object sender, PlayerHealthManager.OnSetIlvunerableEventArgs e)
+    {
+        gameObject.SetActive(!e.isInvulnerable);
+    }
+
+    private void OnHealthChanged(object sender, PlayerHealthManager.OnHealthChangedEventArgs e)
+    {
+        healthFill.fillAmount = e.health;
+    }
+
+}
