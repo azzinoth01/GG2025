8b71c928575d40fee6d4199a120d7dd617f57aea
azzinoth01 <markusdullnig@hotmail.com>
2025-01-25 17:08:58 +0100

    shooting sound effect

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,33 +1,45 @@
-using System;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance { get; private set;}
+    public static GameManager Instance {
+        get; private set;
+    }
+
 
     [SerializeField] private Player player;
     [SerializeField] private GameObject deathScreen;
     private PlayerHealthManager playerHealthManager;
 
-    private void Awake(){
-    if(Instance != null && Instance != this){
+    [SerializeField] private AudioMixer _audioMixer;
+
+
+    public AudioMixer AudioMixer {
+        get {
+            return _audioMixer;
+        }
+    }
+
+    private void Awake() {
+        if (Instance != null && Instance != this) {
             Destroy(this);
-        } else {
+        }
+        else {
             Instance = this;
         }
     }
-    void Start(){
+    void Start() {
         playerHealthManager = player.GetComponent<PlayerHealthManager>();
         playerHealthManager.OnPlayerDied += OnPlayerDied;
     }
-    public void OnPlayerDied()
-    {
+    public void OnPlayerDied() {
         Destroy(player.gameObject);
         deathScreen.SetActive(true);
     }
 
-    public void RestartGame(){
+    public void RestartGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
--- Assets/Scripts/Player/Weapon.cs
+++ Assets/Scripts/Player/Weapon.cs
@@ -5,11 +5,20 @@ public class Weapon : MonoBehaviour
     [SerializeField] private GameObject _aimPiviot;
     [SerializeField] private GameObject _projectileSpawnPoint;
     [SerializeField] private Projectile _projectilePrefab;
+
+    [SerializeField] private AudioClip _shootSound;
     private Vector2 _aimDirection;
 
     public void ShootProjectile() {
         Projectile projectile = Instantiate(_projectilePrefab, _projectileSpawnPoint.transform.position, Quaternion.identity);
         projectile.MoveDirection = _aimDirection;
+
+        GameManager.Instance.AudioMixer.GetFloat("SFX", out float sfxVolume);
+
+        float linearVolume = Mathf.Pow(10, sfxVolume / 20);
+
+        AudioSource.PlayClipAtPoint(_shootSound, _projectileSpawnPoint.transform.position, linearVolume);
+
     }
 
     public void SetAim(Vector2 direction) {
@@ -17,14 +26,6 @@ public class Weapon : MonoBehaviour
 
         float aimAngle = Vector2.SignedAngle(Vector2.right, _aimDirection);
         _aimPiviot.transform.transform.localEulerAngles = new Vector3(0, 0, aimAngle);
-    }
-
-    void Start() {
-
-    }
-
-    // Update is called once per frame
-    void Update() {
 
     }
 }
