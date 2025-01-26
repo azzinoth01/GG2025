f82d07cd91866c90a3259cffdcb638d2f4bcd714
AugustinGressier <augustinv.gressier@orange.fr>
2025-01-26 11:05:37 +0100

    nerge fix

--- Assets/Scripts/Audio/ForceLoadSoundSettings.cs
+++ Assets/Scripts/Audio/ForceLoadSoundSettings.cs
@@ -6,9 +6,5 @@ public class ForceLoadSoundSettings : MonoBehaviour
 
     private void Start() {
         AudioManager.Instance.LoadAudioSettings();
-
-        AudioSource source = gameObject.GetComponent<AudioSource>();
-        AudioMixerGroup mixerGroup = AudioManager.Instance.AudioMixer.FindMatchingGroups("Music")[0]; ;
-        source.outputAudioMixerGroup = mixerGroup;
     }
 }
--- Assets/Scripts/DangerRTPC.cs
+++ Assets/Scripts/DangerRTPC.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class DangerRTPC : MonoBehaviour
 {
-    GameObject enemy;
+    [SerializeField] private GameObject enemy;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        enemy = GameObject.Find("Chaser");
+    //// Start is called once before the first execution of Update after the MonoBehaviour is created
+    //void Start()
+    //{
+    //    enemy = GameObject.Find("Chaser");
 
-    }
+    //}
 
     // Update is called once per frame
     void Update()
