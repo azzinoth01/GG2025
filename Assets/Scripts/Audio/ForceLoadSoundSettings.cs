2a97ea4d6da3ba19f9af156bafeec90cfb79dbac
azzinoth01 <markusdullnig@hotmail.com>
2025-01-26 13:54:17 +0100

    fix volume settings

--- Assets/Scripts/Audio/ForceLoadSoundSettings.cs
+++ Assets/Scripts/Audio/ForceLoadSoundSettings.cs
@@ -4,16 +4,12 @@ public class ForceLoadSoundSettings : MonoBehaviour
 {
 
     private void Start() {
-        AudioManager.Instance.LoadAudioSettings();
+        //  AudioManager.Instance.LoadAudioSettings();
 
-        AudioManager.Instance.AudioMixer.GetFloat("SFX", out float sfxDecibel);
-        AudioManager.Instance.AudioMixer.GetFloat("Music", out float musicDecibel);
+        SaveSettings saveSettings = SaveSettings.LoadFile();
 
-        float sfxVolume = Mathf.Clamp(Mathf.Pow(10, sfxDecibel / 20), 0, 1);
-        float musicVolume = Mathf.Clamp(Mathf.Pow(10, musicDecibel / 20), 0, 1);
-
-        AkSoundEngine.SetRTPCValue("SFXVolume", sfxVolume);
-        AkSoundEngine.SetRTPCValue("MusicVolume", musicVolume);
+        AkSoundEngine.SetRTPCValue("SFXVolume", saveSettings.SfxVolume);
+        AkSoundEngine.SetRTPCValue("MusicVolume", saveSettings.MusicVolume);
 
     }
 }
