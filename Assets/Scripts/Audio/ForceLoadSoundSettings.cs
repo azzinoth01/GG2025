55f92cc330933abbc735d7766393ebd82ae12ee4
azzinoth01 <markusdullnig@hotmail.com>
2025-01-26 13:30:23 +0100

    set slider volumes

--- Assets/Scripts/Audio/ForceLoadSoundSettings.cs
+++ Assets/Scripts/Audio/ForceLoadSoundSettings.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
-using UnityEngine.Audio;
 
 public class ForceLoadSoundSettings : MonoBehaviour
 {
 
     private void Start() {
         AudioManager.Instance.LoadAudioSettings();
+
+        AudioManager.Instance.AudioMixer.GetFloat("SFX", out float sfxDecibel);
+        AudioManager.Instance.AudioMixer.GetFloat("Music", out float musicDecibel);
+
+        float sfxVolume = Mathf.Clamp(Mathf.Pow(10, sfxDecibel / 20), 0, 1);
+        float musicVolume = Mathf.Clamp(Mathf.Pow(10, musicDecibel / 20), 0, 1);
+
+        AkSoundEngine.SetRTPCValue("SFXVolume", sfxVolume);
+        AkSoundEngine.SetRTPCValue("MusicVolume", musicVolume);
+
     }
 }
